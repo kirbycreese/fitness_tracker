a42ab65446ee47aa11ef4ebdfd9f3b696e65d152
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-01-24 11:33:56 -0500

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workouttracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html-routes.js"));
